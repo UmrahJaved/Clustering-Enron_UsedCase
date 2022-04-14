13179a91b6d761f3ebe209807156b5a90b28fb1c
Jari Erämaa <jari.eramaa@gmail.com>
2022-04-14 15:42:03 +0200

    started hierarchy, but testing an efficient wy in Jupyter notebook

--- calc_lda.py
+++ calc_lda.py
@@ -16,6 +16,7 @@ import dill
 from typing import Union
 import warnings
 
+from regex import D
 
 
 #Dropbox account: umrahjaved@yahoo.com
@@ -23,34 +24,28 @@ import warnings
 
 MALLET_PATH = './mallet-2.0.8/bin/mallet'
 
-def load_files(test : bool = True) ->  Union[list, list, Object]:
+def load_files(test : bool = True) ->  Union[list, pd.DataFrame]:
     """
     Load the files from the dataframe
     param 
     :test: True if the test data is to be loaded
-    :list: lemmatized data
-    :list: enron emails body as a list
-    :Object: corpora module
+    return:
+    :list: list of lemmatized emails
+    :pd.DataFrame: enron original email dataframe
     """
+    # We work with these. These are the one's that define what data we are using
     with open("./calc_data/data_lemmatized.pkl", 'rb') as my_pickle:
         data_lemmatized = pickle.load(my_pickle)
 
-    with open("./calc_data/data.pkl", 'rb') as my_pickle:
-        data = pickle.load(my_pickle)
-
+    # This is needed when result is made
     with open("./calc_data/enron_df.pkl", 'rb') as my_pickle:
         enron = pickle.load(my_pickle)
-    
-    #corpora is a dill file (module cannot be pickled)
-    with open("./calc_data/corpora.dill", 'rb') as f:
-        corpora = dill.load(f)
-    
+
     if test:
         data_lemmatized = data_lemmatized[:1000]
-        data = data[:1000]
+        #data = data[:1000]
         enron = enron[:1000]
-
-    return data_lemmatized, data, corpora
+    return data_lemmatized, enron
 
 
 def create_id2word(data_lemmatized : list) -> gensim.corpora.Dictionary:
@@ -97,7 +92,7 @@ def calculate_lda_model(corpus : gensim.corpora.Dictionary, id2word : gensim.cor
                                                 per_word_topics=True)
     return lda_model
 
-def calculate_score(corpus, lda_model, id2word):
+def calculate_score(corpus, lda_model, id2word, data_lemmatized):
     perplexity = lda_model.log_perplexity(corpus) # a measure of how good the model is. lower the better.
     print('\nPerplexity: ', perplexity)
 
@@ -121,23 +116,80 @@ def visualize_topic(lda_model : gensim.models.ldamodel.LdaModel, corpus : list, 
     pyLDAvis.display(vis)
     return vis
 
-    
-if __name__ == '__main__':
-    data_lemmatized, data, corpora = load_files(test=True)
-    texts = data_lemmatized
-    print(data_lemmatized[1])
-    print("Files loaded")
-    id2word = create_id2word(data_lemmatized)
+def categorise_emails(ldamodel : gensim.models.ldamodel.LdaModel, corpus : list, texts : list) -> pd.DataFrame:
+    """
+    Creates a dataframe that contains information what is the dominant topic to this email. Also how
+    much of the email is in the topic. 
+    param:
+    :gensim.models.ldamodel.LdaModel: LDA model
+    :list: corpus
+    :list: texts (lemmatized emails)
+    """
+    sent_topics_df = pd.DataFrame()
+    # Get main topic in each document
+    for i, row in enumerate(ldamodel[corpus]):
+        row = sorted(
+            row[0], key=lambda x: (x[1]), 
+            reverse=True)
+        # Get the Dominant topic, Perc Contribution and Keywords for each document
+        for j, (topic_num, prop_topic) in enumerate(row):
+            if j == 0:  # => dominant topic
+                wp = ldamodel.show_topic(topic_num)
+                topic_keywords = ", ".join([word for word, prop in wp])
+                sent_topics_df = sent_topics_df.append(pd.Series([int(topic_num), round(prop_topic,4), topic_keywords]), ignore_index=True)
+            else:
+                break
+    sent_topics_df.columns = ['Dominant_Topic', 'Perc_Contribution', 'Topic_Keywords']
 
-    print("id2word created")
+    # Add original text to the end of the output NOT NEEDED
+    #contents = pd.Series(texts)
+    #sent_topics_df = pd.concat([sent_topics_df, contents], axis=1)
+    return(sent_topics_df)
+
+def divide_df(cat_df :pd.DataFrame) -> Union [pd.DataFrame, pd.DataFrame]:
+    """
+    Divide the dataframe into two dataframes, one for the emails that are in the topic 0 and one for the emails that are in the
+    topic 1.
+    param:
+    :pd.DataFrame: dataframe
+    :return: pd.DataFrame, pd.DataFrame
+    """
+    cat_df_topic = cat_df[cat_df['Dominant_Topic'] == 0]
+    cat_df_not_topic = cat_df[cat_df['Dominant_Topic'] != 0]
+    return cat_df_topic, cat_df_not_topic
+
+def sub_level():
+    #testing in Jupyter how this should be done efficiently
+    pass
+
+def add_enron_columns(cat_emails : pd.DataFrame, enron : pd.DataFrame) -> pd.DataFrame:
+    cat_emails['ID'] = enron['Message-ID']
+    cat_emails['Subject'] = enron['Subject']
+    cat_emails['enron_content'] = enron['content']
+    cat_emails['From'] = enron['From']
+    cat_emails['To'] = enron['To']
+    cat_emails['Cc'] = enron['X-cc']
+    cat_emails['Bcc'] = enron['X-bcc']
+
+
+def first_level():
+    data_lemmatized, enron = load_files(test=True)
+    id2word = create_id2word(data_lemmatized)
     corpus = calc_term_doc_frequency(id2word, data_lemmatized)
-    print("corpus created")
     lda_model = calculate_lda_model(corpus, id2word, 2)
-    print("lda model calculated")
-    calculate_score(corpus, lda_model, id2word)
-    print("score calculated")
-    #visualize_topic(lda_model, corpus, id2word)
-    print("visualized")
-    print("Done")
+    categorised_emails = categorise_emails(lda_model, corpus, data_lemmatized)
+    categorised_emails = add_enron_columns(categorised_emails, enron)
+
+
+
+    #calculate_score(corpus, lda_model, id2word, data_lemmatized)
+
+
+
+
+    
+if __name__ == '__main__':    
+    first_level()
+
 
 
